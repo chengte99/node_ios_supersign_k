39b8faae8750b756ed537ae4f91bd968cadf2e87
chengte99 <chengte99@gmail.com>
2019-11-05 11:38:27 +0800

    安裝redis

---
+++ database/redis_center.js
@@ -0,0 +1,19 @@
+var redis = require("redis");
+var util = require("util");
+
+var log = require("../utils/log");
+
+var redis_client = null;
+
+function connect_to_server(host, port, db_index){
+    redis_client = redis.createClient({
+        host: host,
+        port: port,
+        db: db_index
+    });
+
+    redis_client.on("error", function(err){
+        log.error(err);
+    });
+}
+
